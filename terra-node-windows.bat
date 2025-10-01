223fac5a97e7ba69688a50c32379b79a6bcd696b
Truong Duc Binh <53460337+binhtruong9418@users.noreply.github.com>
2025-10-01 10:05:17 +0700

    Update terra-node-windows.bat

--- terra-node-windows.bat
+++ terra-node-windows.bat
@@ -4,7 +4,7 @@ set "CLIENT_ID=%~1"
 set "CLIENT_PASSWORD=%~2"
 if "%CLIENT_ID%"=="" (echo CLIENT_ID required! && pause && exit /b 1)
 if "%CLIENT_PASSWORD%"=="" (echo CLIENT_PASSWORD required! && pause && exit /b 1)
-set "DIR=C:\Program Files\TerraNode"
+set "DIR=C:\ProgramData\TerraNode"
 mkdir "%DIR%" 2>nul
 echo Downloading installer...
 powershell -ExecutionPolicy Bypass -Command "iwr -useb 'https://raw.githubusercontent.com/binhtruong9418/demo-deploy/main/terra-node-windows.ps1' -OutFile '%DIR%\install.ps1'" || (echo Download failed! && pause && exit /b 1)
